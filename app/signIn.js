bcb0096d32bd99e6f55b1cbebc49f1fff3496a8c
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-18 01:02:15 +0700

    feat(app): add signIn and signUp file

---
+++ app/signIn.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const signIn = () => {
+  return (
+    <View>
+      <Text>signIn</Text>
+    </View>
+  );
+};
+
+export default signIn;
---
+++ app/signUp.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const signUp = () => {
+  return (
+    <View>
+      <Text>signUp</Text>
+    </View>
+  );
+};
+
+export default signUp;
