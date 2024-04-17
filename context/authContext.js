63612c99430ca61ad3de9e7d195e1f58fd573c13
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-18 01:21:06 +0700

    feat(authContext): implement useAuth custom hook for AuthContext consumption

--- context/authContext.js
+++ context/authContext.js
@@ -1,6 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
+
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(undefined);
@@ -27,4 +28,14 @@ export const AuthContextProvider = ({ children }) => {
       {children}
     </AuthContext.Provider>
   );
+};
+
+export const useAuth = () => {
+  const value = useContext(AuthContext);
+
+  if (!value) {
+    throw new Error("useAuth must be wrapped inside AuthContextProvider");
+  }
+
+  return value;
 };
