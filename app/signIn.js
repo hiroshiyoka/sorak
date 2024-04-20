452e554091d9360640867ebd323c8be12ad893ff
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-21 03:11:51 +0700

    feat(signin): add password input field with styling

--- app/signIn.js
+++ app/signIn.js
@@ -43,6 +43,19 @@ const signIn = () => {
               placeholderTextColor={"gray"}
             />
           </View>
+
+          <View
+            style={{ height: hp(7) }}
+            className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+          >
+            <Octicons name="lock" size={hp(2.7)} color="gray" />
+            <TextInput
+              style={{ fontSize: hp(2) }}
+              className="flex-1 font-semibold text-neutral-700"
+              placeholder="Password"
+              placeholderTextColor={"gray"}
+            />
+          </View>
         </View>
       </View>
     </View>
