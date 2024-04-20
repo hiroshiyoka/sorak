f6bf9cf6195e1fa714d850392ca2ca6af2d37af9
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-21 02:55:42 +0700

    feat(signin): add StatusBar and login image to signIn screen

--- app/signIn.js
+++ app/signIn.js
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { View, Image } from "react-native";
 
 const signIn = () => {
   return (
-    <View className="pt-20">
-      <Text>signIn</Text>
+    <View className="flex-1">
+      <StatusBar style="dark" />
+      <View className="flex-1 gap-12">
+        <View className="items-center">
+          <Image source={require("../assets/images/login.png")} />
+        </View>
+      </View>
     </View>
   );
 };
