2797374578e73e416e20e69aa7485e544c987d3d
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-21 03:00:54 +0700

    feat(signin): give sign-in image responsive padding and sizing

--- app/signIn.js
+++ app/signIn.js
@@ -1,13 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { View, Image } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const signIn = () => {
   return (
     <View className="flex-1">
       <StatusBar style="dark" />
-      <View className="flex-1 gap-12">
+      <View
+        style={{ paddingTop: hp(8), paddingHorizontal: wp(5) }}
+        className="flex-1 gap-12"
+      >
+        {/* Sign In Image */}
         <View className="items-center">
-          <Image source={require("../assets/images/login.png")} />
+          <Image
+            style={{ height: hp(25) }}
+            resizeMode="contain"
+            source={require("../assets/images/login.png")}
+          />
         </View>
       </View>
     </View>
