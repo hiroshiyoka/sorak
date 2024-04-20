af71262588ee085efbad328f4892093b66588618
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-21 03:09:14 +0700

    feat(signin): add email input field with styling

--- app/signIn.js
+++ app/signIn.js
@@ -1,5 +1,6 @@
+import { Octicons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
-import { View, Image } from "react-native";
+import { View, Image, TextInput } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -20,6 +21,28 @@ const signIn = () => {
             resizeMode="contain"
             source={require("../assets/images/login.png")}
           />
+        </View>
+
+        <View className="gap-10">
+          <Text
+            style={{ fontSize: hp(4) }}
+            className="font-bold tracking-wider text-center text-neutral-800"
+          >
+            Sign In
+          </Text>
+          {/* Inputs */}
+          <View
+            style={{ height: hp(7) }}
+            className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+          >
+            <Octicons name="mail" size={hp(2.7)} color="gray" />
+            <TextInput
+              style={{ fontSize: hp(2) }}
+              className="flex-1 font-semibold text-neutral-700"
+              placeholder="Email Address"
+              placeholderTextColor={"gray"}
+            />
+          </View>
         </View>
       </View>
     </View>
