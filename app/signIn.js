4f2f5f7697e0a2fe9ed070d89127a11e75048747
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-21 03:14:02 +0700

    feat(signin): add gap view for email and password inputs

--- app/signIn.js
+++ app/signIn.js
@@ -31,30 +31,32 @@ const signIn = () => {
             Sign In
           </Text>
           {/* Inputs */}
-          <View
-            style={{ height: hp(7) }}
-            className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
-          >
-            <Octicons name="mail" size={hp(2.7)} color="gray" />
-            <TextInput
-              style={{ fontSize: hp(2) }}
-              className="flex-1 font-semibold text-neutral-700"
-              placeholder="Email Address"
-              placeholderTextColor={"gray"}
-            />
-          </View>
+          <View className="gap-4">
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+            >
+              <Octicons name="mail" size={hp(2.7)} color="gray" />
+              <TextInput
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Email Address"
+                placeholderTextColor={"gray"}
+              />
+            </View>
 
-          <View
-            style={{ height: hp(7) }}
-            className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
-          >
-            <Octicons name="lock" size={hp(2.7)} color="gray" />
-            <TextInput
-              style={{ fontSize: hp(2) }}
-              className="flex-1 font-semibold text-neutral-700"
-              placeholder="Password"
-              placeholderTextColor={"gray"}
-            />
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+            >
+              <Octicons name="lock" size={hp(2.7)} color="gray" />
+              <TextInput
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Password"
+                placeholderTextColor={"gray"}
+              />
+            </View>
           </View>
         </View>
       </View>
