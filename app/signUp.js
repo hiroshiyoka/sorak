c679fd2a4b509694f5d5d3ea1194c7e62e14b5bf
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-24 00:46:55 +0700

    feat(signup): add sign up component and functionality with form validation

--- app/signUp.js
+++ app/signUp.js
@@ -1,9 +1,171 @@
-import { View, Text } from "react-native";
+import { Feather, Octicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
+import {
+  View,
+  Image,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Pressable,
+  Alert,
+} from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Loading from "@/components/Loading";
 
 const signUp = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const usernameRef = useRef("");
+  const profileRef = useRef("");
+
+  const handleRegister = async () => {
+    if (
+      !emailRef.current ||
+      !passwordRef.current ||
+      !usernameRef.current ||
+      !profileRef.current
+    ) {
+      Alert.alert("Sign Up", "Please fill all the fields");
+      return;
+    }
+  };
+
   return (
-    <View>
-      <Text>signUp</Text>
+    <View className="flex-1">
+      <StatusBar style="dark" />
+      <View
+        style={{ paddingTop: hp(7), paddingHorizontal: wp(5) }}
+        className="flex-1 gap-12"
+      >
+        {/* Sign In Image */}
+        <View className="items-center">
+          <Image
+            style={{ height: hp(20) }}
+            resizeMode="contain"
+            source={require("../assets/images/register.png")}
+          />
+        </View>
+
+        <View className="gap-10">
+          <Text
+            style={{ fontSize: hp(4) }}
+            className="font-bold tracking-wider text-center text-neutral-800"
+          >
+            Sign Up
+          </Text>
+          {/* Inputs */}
+          <View className="gap-4">
+            {/* Username Input */}
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+            >
+              <Feather name="user" size={hp(2.7)} color="gray" />
+              <TextInput
+                onChangeText={(value) => (usernameRef.current = value)}
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Username"
+                placeholderTextColor={"gray"}
+              />
+            </View>
+
+            {/* Email Input */}
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+            >
+              <Octicons name="mail" size={hp(2.7)} color="gray" />
+              <TextInput
+                onChangeText={(value) => (emailRef.current = value)}
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Email Address"
+                placeholderTextColor={"gray"}
+              />
+            </View>
+
+            {/* Password Input */}
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+            >
+              <Octicons name="lock" size={hp(2.7)} color="gray" />
+              <TextInput
+                onChangeText={(value) => (passwordRef.current = value)}
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Password"
+                secureTextEntry
+                placeholderTextColor={"gray"}
+              />
+            </View>
+
+            {/* Profile Input */}
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl"
+            >
+              <Feather name="image" size={hp(2.7)} color="gray" />
+              <TextInput
+                onChangeText={(value) => (profileRef.current = value)}
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Profile URL"
+                placeholderTextColor={"gray"}
+              />
+            </View>
+
+            {/* Submit Button */}
+            <View>
+              {loading ? (
+                <View className="flex-row justify-center">
+                  <Loading size={hp(6.5)} />
+                </View>
+              ) : (
+                <TouchableOpacity
+                  onPress={handleRegister}
+                  style={{ height: hp(6.5) }}
+                  className="bg-indigo-500 rounded-xl justify-center items-center"
+                >
+                  <Text
+                    style={{ fontSize: hp(2.7) }}
+                    className="text-white font-bold tracking-wider"
+                  >
+                    Sign Up
+                  </Text>
+                </TouchableOpacity>
+              )}
+            </View>
+
+            {/* Sign Up Field */}
+            <View className="flex-row justify-center">
+              <Text
+                style={{ fontSize: hp(1.8) }}
+                className="font-semibold text-neutral-500"
+              >
+                Already have an account?{" "}
+              </Text>
+              <Pressable onPress={() => router.push("signIn")}>
+                <Text
+                  style={{ fontSize: hp(1.8) }}
+                  className="font-bold text-indigo-500"
+                >
+                  Sign In
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
