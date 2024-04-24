d316d769f23dd23173cfe73ffe02613c23e6d324
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-25 01:02:45 +0700

    feat(firebase): initialize firebase with firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFKFGVRpvAteiRVQLKtGI5trn2_YJMSQM",
+  authDomain: "sorak-firebase.firebaseapp.com",
+  projectId: "sorak-firebase",
+  storageBucket: "sorak-firebase.appspot.com",
+  messagingSenderId: "210908846928",
+  appId: "1:210908846928:web:d5c8df09a4976aecabea55",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
