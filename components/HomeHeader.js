a106d5f8dc1e7bc0ec74a25e4fe1dedab57d2cbc
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-26 00:32:12 +0700

    feat(homeheader): add safe area handling and styling to HomeHeader component

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,11 +1,23 @@
 import { View, Text, Platform } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 const ios = Platform.OS == "ios";
 
 const HomeHeader = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View
+      style={{ paddingTop: ios ? top : top + 10 }}
+      className="flex-row justify-between px-5 bg-indigo-400 pb-6 rounded-b-3xl shadow"
+    >
+      <View>
+        <Text
+          style={{ fontSize: hp(3) }}
+          className="font-medium text-white"
+        ></Text>
+      </View>
     </View>
   );
 };
