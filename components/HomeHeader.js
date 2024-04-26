e00135934b9e017416f716d9892574794d230342
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-27 06:00:30 +0700

    feat(homeheader): add image view from expo-image

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,13 @@
 import { View, Text, Platform } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { useAuth } from "@/context/authContext";
+import { Image } from "expo-image";
 
 const ios = Platform.OS == "ios";
 
 const HomeHeader = () => {
+  const { user } = useAuth();
   const { top } = useSafeAreaInsets();
 
   return (
@@ -16,6 +19,15 @@ const HomeHeader = () => {
         <Text style={{ fontSize: hp(3) }} className="font-medium text-white">
           Chats
         </Text>
+      </View>
+
+      <View>
+        <Image
+          style={{ height: hp(4.3), aspectRatio: 1, borderRadius: 100 }}
+          source={user?.profileUrl}
+          placeholder={blurhash}
+          transition={500}
+        />
       </View>
     </View>
   );
