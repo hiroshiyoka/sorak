1a6723ced510a94c2963b61fa2b839dd5041a711
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-28 01:55:14 +0700

    feat(chatitem): add ChatItem component to display user information in the chat interface

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
+import { TouchableOpacity, Image, Text, View } from "react-native";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
-const ChatItem = () => {
+const ChatItem = ({ item }) => {
   return (
-    <View>
-      <Text>ChatItem</Text>
-    </View>
+    <TouchableOpacity className="flex-row justify-between mx-4 items-center gap-3 mb-4 pb-2 border-b border-neutral-200">
+      <Image
+        source={require("../assets/images/avatar.png")}
+        style={{ height: hp(6), width: hp(6) }}
+        className="rounded-full"
+      />
+
+      {/* Name, Time, and Last Message */}
+      <View className="flex-1 gap-1">
+        {/* Name and Time */}
+        <View className="flex-row justify-between">
+          <Text
+            style={{ fontSize: hp(1.8) }}
+            className="font-semibold text-neutral-800"
+          >
+            Parhan
+          </Text>
+          <Text
+            style={{ fontSize: hp(1.6) }}
+            className="font-medium text-neutral-500"
+          >
+            Time
+          </Text>
+        </View>
+
+        {/* Last Message */}
+        <Text
+          style={{ fontSize: hp(1.6) }}
+          className="font-medium text-neutral-500"
+        >
+          Last Message
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
