768d687bf5141993373a7ad595d9a2aba94d6bf0
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-28 02:00:57 +0700

    feat(chatitem): add router and noBorder props to chatitem component

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,9 +1,13 @@
 import { TouchableOpacity, Image, Text, View } from "react-native";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
-const ChatItem = ({ item }) => {
+const ChatItem = ({ item, router, noBorder }) => {
   return (
-    <TouchableOpacity className="flex-row justify-between mx-4 items-center gap-3 mb-4 pb-2 border-b border-neutral-200">
+    <TouchableOpacity
+      className={`flex-row justify-between mx-4 items-center gap-3 mb-4 pb-2 ${
+        noBorder ? "" : "border-b border-neutral-200"
+      }`}
+    >
       <Image
         source={require("../assets/images/avatar.png")}
         style={{ height: hp(6), width: hp(6) }}
