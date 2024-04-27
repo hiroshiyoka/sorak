f05960cae7e422e055df2479f3ea396f2f20ee8f
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-28 01:41:13 +0700

    feat(chatlist): add styling to ChatList component and add Flatlist component

--- components/ChatList.js
+++ components/ChatList.js
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
+import { View, FlatList } from "react-native";
 
 const ChatList = ({ users }) => {
   return (
-    <View>
-      <Text>ChatList</Text>
+    <View className="flex-1">
+      <FlatList
+        data={users}
+        contentContainerStyle={{ flex: 1, paddingVertical: 25 }}
+        keyExtractor={(item) => Math.random()}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
