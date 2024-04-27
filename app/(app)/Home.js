b71403790854f56d1bd6fd51f487e977585321e3
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-04-28 01:36:30 +0700

    feat(home): add conditional rendering based on the length of users in Home component

--- app/(app)/Home.js
+++ app/(app)/Home.js
@@ -1,9 +1,23 @@
-import { View } from "react-native";
+import ChatList from "@/components/ChatList";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+
   return (
     <View className="flex-1 bg-white">
-      <Text>Home</Text>
+      <StatusBar style="light" />
+
+      {users.length > 0 ? (
+        <ChatList users={users} />
+      ) : (
+        <View className="flex items-center" style={{ top: hp(30) }}>
+          <ActivityIndicator size="large" />
+        </View>
+      )}
     </View>
   );
 };
